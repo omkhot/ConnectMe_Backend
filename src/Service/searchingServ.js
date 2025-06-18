189140dba795981f4b33b139f2e41b212d143246
Om Khot <iithomkhot@gmail.com>
2025-06-18 17:27:11 +0530

    searching ser layer code changed

--- src/Service/searchingServ.js
+++ src/Service/searchingServ.js
@@ -1,20 +1,20 @@
 import { searchUserRepo } from "../Repository/searchingRepo.js";
-import NotFoundError from "../Utils/notFoundError.js";
+// import NotFoundError from "../Utils/notFoundError.js";
 
 
 async function searchUserServ(userDetailsQuery) {
     try {
         const user = await searchUserRepo(userDetailsQuery);
         if(user.length === 0){
-            throw new NotFoundError([userDetailsQuery], "user");
+            // throw new NotFoundError([userDetailsQuery], "user");
         }
         return user;
     } catch (error) {
         console.error("Error in searchUserServ:", error);
-        if (error instanceof NotFoundError) {
-            throw error;
-        }
-        else throw error; // Rethrow the error for further handling if needed
+        // if (error instanceof NotFoundError) {
+        //     throw error;
+        // }
+        // else throw error; // Rethrow the error for further handling if needed
     }
 }
 
