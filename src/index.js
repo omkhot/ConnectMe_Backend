d0ec4802c47a8e98cc8b36bcac99c638b9e517f0
Om Khot <iithomkhot@gmail.com>
2025-06-18 18:10:52 +0530

    index.js file changed

--- src/index.js
+++ src/index.js
@@ -34,13 +34,7 @@ app.use(passport.initialize());
 const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:5173"];
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }));
 
